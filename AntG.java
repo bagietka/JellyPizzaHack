7f94bd9b2188f0f7b1c7fb80dbeb84cdbda1bff6
e-dzia <226000@student.pwr.edu.pl>
2016-12-16 21:26:07 +0100

    Graphic Ant Ready

---
+++ AntG.java
@@ -0,0 +1,36 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class AntG {
+	int x;
+	int y;
+	char color;
+	
+	AntG(int x, int y, char color){
+		this.x = x;
+		this.y = y;
+		this.color = color;
+		
+	}
+	
+	public void drawMe(Graphics g){
+		switch(color){
+			case 'r': 
+				g.setColor(Color.RED);
+				break;
+			case 'g':
+				g.setColor(Color.GREEN);
+				break;
+			case 'b':
+				g.setColor(Color.BLUE);
+				break;
+			default:
+				g.setColor(Color.BLACK);
+				break;
+		}
+		
+		int radius = 6;
+		g.fillOval(x, y, radius, radius);
+	}
+
+}
