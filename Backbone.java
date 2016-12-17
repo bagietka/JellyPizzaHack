358cb3cb6df6c02bf23ebaf66914a08d6c244811
e-dzia <226000@student.pwr.edu.pl>
2016-12-17 05:10:41 +0100

    Lepsze współrzędne, ale wciąż nie działa :(

--- Backbone.java
+++ Backbone.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -14,6 +15,7 @@ public class Backbone {
 	static final int height = (int) java.awt.Toolkit.getDefaultToolkit().getScreenSize().getHeight();
 	int n; //liczba kolumn = 2*n; n-k¹t
 	int version;
+	double alpha;
 	List<List<Path>> graph;
 	List<Ant> ants;
 	
@@ -34,17 +36,18 @@ public class Backbone {
 		int y;
 		switch(version){
 			case 1:
+				listOfPeaks.clear();
 				distanceX = width/(2*n+3);
 				distanceY = height/4;
 				x = 50;
 				y = height/2-50;
-				listOfPeaks.add(new PeakG(x,y,number));
+				listOfPeaks.add(new PeakG(x,y,0,number));
 				listOfPeaks.get(number++).drawMe(g);
 				for (int i = 0; i < 2*n; i++){
 					x += distanceX;
 					y = 50;
 					for(int j = 0; j < 4; j++){
-						listOfPeaks.add(new PeakG(x,y,number));
+						listOfPeaks.add(new PeakG(x,y,0,number));
 						listOfPeaks.get(number++).drawMe(g);
 						y += distanceY;
 					}
@@ -52,39 +55,48 @@ public class Backbone {
 				x += distanceX;
 				y = 50;
 				for(int j = 0; j < 4; j++){
-					listOfPeaks.add(new PeakG(x,y,number));
+					listOfPeaks.add(new PeakG(x,y,0,number));
 					listOfPeaks.get(number++).drawMe(g);
 					y += distanceY;
 				}
 				x += distanceX;
 				y = height/2-50;
-				listOfPeaks.add(new PeakG(x,y,number));
+				listOfPeaks.add(new PeakG(x,y,0,number));
 				listOfPeaks.get(number++).drawMe(g);
 				break;
 			case 2:
-				x = 300; y = 300;
-				for (int i = 0; i < n; i++){
-					listOfPeaks.add(new PeakG(x,y,number));
+				listOfPeaks.clear();
+				x = 300; y = height/2;
+				alpha = 0;
+				for (int i = 0; i < n; i++){ //TODO n+1, bo Verticles +1 i inaczej nie dzia�a :p Rudy!!!
+					listOfPeaks.add(new PeakG(x,y,alpha,number));
 					listOfPeaks.get(number++).drawMe(g);
 					Graphics2D g2d = (Graphics2D) g;
 					g2d.rotate(2*Math.PI/n, width/2, height/2);
+					alpha += 2*Math.PI/n;
 				}
 				break;
 			case 3:
-				
+				listOfPeaks.clear();
 				break;
 			default:
 				g.drawString("Zle dane, ziomek. No wez sie postaraj, gosciu", width/2, height/2);
 		}
 		
 		for (List<Path> element : graph){
-						for (Path p: element){
-							//stwórz ³¹czenie miêdzy dwoma Peaksami
-						}
-					}
-					for (Ant a: ants){
-						
-					}
+			for (Path p: element){
+				Line l = new Line(p.from,p.to, listOfPeaks, p.pheromone);
+				l.drawMe(g);
+			}
+		}
+	    for (Ant a: ants){
+	    	//System.out.println(listOfPeaks.get(a.position).getX() + " " + listOfPeaks.get(a.position).getY());
+	    	x = listOfPeaks.get(a.position).getX();
+	    	y = listOfPeaks.get(a.position).getY();
+	    	AntG ag = new AntG(x,y,'r');
+	    	ag.drawMe(g);
+	    	
+	    }
 	}
 	
 	public List<PeakG> getList(Graphics g)
--- Line.java
+++ Line.java
@@ -12,12 +12,12 @@ public class Line {
 	private int thickness;
 	List<PeakG> listOfPeaks = new ArrayList<PeakG>();
 	
-	Line(int from, int to, List<PeakG> listOfPeaks, int thickness)
+	Line(int from, int to, List<PeakG> listOfPeaks, double thickness)
 	{
 		this.from = from;
 		this.to = to;
 		this.listOfPeaks = listOfPeaks;
-		this.thickness = thickness;
+		this.thickness = (int) thickness;
 	}
 	
 	public void drawMe(Graphics g) {
@@ -25,17 +25,17 @@ public class Line {
 		Graphics2D g2d = (Graphics2D) g;
 		g2d.setStroke(new BasicStroke(thickness));
 		
-		for (int i = 0; i < 10; i++)
+		/*for (int i = 0; i < 10; i++)
 		{
 			System.out.println(i);
-			System.out.println(listOfPeaks.get(i).getXfromN() + " " + listOfPeaks.get(i).getYfromN());
+			System.out.println(listOfPeaks.get(i).getX() + " " + listOfPeaks.get(i).getY());
 			
-		}
+		}*/
 		
-		System.out.println(from + " " + to);		
+		//System.out.println(from + " " + to);		
 		//g2d.drawLine(listOfPeaks.get(from).getXfromN(from), listOfPeaks.get(from).getYfromN(from), listOfPeaks.get(to).getXfromN(to), listOfPeaks.get(to).getYfromN(to));
-		System.out.println(listOfPeaks.get(from).getXfromN() + " " + listOfPeaks.get(from).getYfromN() + " " + listOfPeaks.get(to).getXfromN() + " " + listOfPeaks.get(to).getYfromN());
-		g2d.drawLine(listOfPeaks.get(from).getXfromN(), listOfPeaks.get(from).getYfromN(), listOfPeaks.get(to).getXfromN(), listOfPeaks.get(to).getYfromN());
+		//System.out.println(listOfPeaks.get(from).getX() + " " + listOfPeaks.get(from).getY() + " " + listOfPeaks.get(to).getX() + " " + listOfPeaks.get(to).getY());
+		g2d.drawLine(listOfPeaks.get(from).getX(), listOfPeaks.get(from).getY(), listOfPeaks.get(to).getX(), listOfPeaks.get(to).getY());
 		g2d.setStroke(new BasicStroke(1));
 		//g.drawLine(x1, y1, x2, y2);
 	}
